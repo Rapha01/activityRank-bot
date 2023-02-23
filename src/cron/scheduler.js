b121bda8d26258d17146d617a4e5a8c1ae5721c6
Rapha01 <sebashatt@gmail.com>
2023-02-23 03:51:49 +0100

    Fix users not getting patreon benefits - paginate patreon pledges.

--- src/cron/scheduler.js
+++ src/cron/scheduler.js
@@ -46,7 +46,7 @@ exports.start = (manager) => {
           } catch (e) { console.log(e); }
         });
       }
-
+      await patreon_api.updatePatrons();
       resolve();
     } catch (e) { reject(e); }
   });
--- src/util/patreon_api.js
+++ src/util/patreon_api.js
@@ -9,41 +9,51 @@ exports.updatePatrons = async function() {
   console.log('Updating Patrons');
 
   let res;
+  let nextUrl = encodeURI('https://www.patreon.com/api/oauth2/v2/campaigns/2925711/members?include=user,currently_entitled_tiers,address&fields[member]=full_name,is_follower,last_charge_date,last_charge_status,lifetime_support_cents,currently_entitled_amount_cents,patron_status&fields[tier]=amount_cents,created_at,description,discord_role_ids,edited_at,patron_count,published,published_at,requires_shipping,title,url&fields[user]=social_connections');
+  let apiMemberData = [];
+
   try {
-    res = await axios.get(encodeURI('https://www.patreon.com/api/oauth2/v2/campaigns/2925711/members?include=user,currently_entitled_tiers,address&fields[member]=full_name,is_follower,last_charge_date,last_charge_status,lifetime_support_cents,currently_entitled_amount_cents,patron_status&fields[tier]=amount_cents,created_at,description,discord_role_ids,edited_at,patron_count,published,published_at,requires_shipping,title,url&fields[user]=social_connections'), { 
-      headers: { 'Authorization': 'Bearer ' + keys.patreonAccessToken  }
-    });
+    while (nextUrl) {
+      res = await axios.get(nextUrl, { 
+        headers: { 'Authorization': 'Bearer ' + keys.patreonAccessToken  }
+      });
+      
+      apiMemberData = apiMemberData.concat(res.data.data);
+      
+      if (res.data.links && res.data.links.next)
+        nextUrl = res.data.links.next;
+      else
+        nextUrl = null;
+    }
   } catch (error) {
-    throw error.code;
+    if (error.code) throw error.code;
+    else throw error;
   }
 
-
   /*
-  * Parse Patreon API. Create activePledges array: Contains all currently active pledges with untilDate, tier and discord connection.
+  * Parse Patreon API. Create entitledPledgesWithDiscord array: Contains all currently active pledges with untilDate, tier and discord connection.
   */
-  console.log(' -------------- Patreon result.data -------------- ');
-  console.log(res.data);
 
-  let activePledges = [];
-  for (let member of res.data.data) {
+  let entitledPledgesWithDiscord = [];
+  for (let member of apiMemberData) {
     try {
       let newPledge = {};
+
       // Set patreonUserId
-      
       newPledge.patreonUserId = member.relationships.user.data.id;
 
       // Set tier. Ignore if no currently entitled tier.
-      if (member.attributes.currently_entitled_amount_cents == '149') {
-        newPledge.tier = 1;
-      } else if (member.attributes.currently_entitled_amount_cents == '349') {
-        newPledge.tier = 2;
-      } else if (member.attributes.currently_entitled_amount_cents == '1449') {
+      if (member.attributes.currently_entitled_amount_cents >= '1449') {
         newPledge.tier = 3;
-      } else
+      } else if (member.attributes.currently_entitled_amount_cents >= '349') {
+        newPledge.tier = 2;
+      } else if (member.attributes.currently_entitled_amount_cents >= '149') {
+        newPledge.tier = 1;
+      } else 
         continue;
 
       // Ignore if not paid.
-      if (member.attributes.last_charge_status != 'Paid')
+      if (member.attributes.last_charge_status != 'Paid') 
         continue;
       
       // Set dates
@@ -56,17 +66,14 @@ exports.updatePatrons = async function() {
       let user = res.data.included.find(inc => inc.type == 'user' && inc.id == newPledge.patreonUserId);
       if (user && user.attributes.social_connections.discord)
         newPledge.discordUserId = user.attributes.social_connections.discord.user_id
-      else
+      else 
         continue;
-
-      activePledges.push(newPledge);
+       
+      entitledPledgesWithDiscord.push(newPledge);
     } catch (error) {
       console.log('Patreon Api parsing error for member '+ member.attributes.full_name + ': ' + error);
     }
   }
-
-  console.log(' -------------- Patreon parsed activePledges -------------- ');
-  console.log(activePledges);
 
   /*
   * Update DB. Use activePledges to update DB (if information of pledge and DB differ)
@@ -74,7 +81,7 @@ exports.updatePatrons = async function() {
 
   const usersWithActivePledge = await shardDb.queryAllHosts(`SELECT * FROM user WHERE patreonTier > 0 && patreonTierUntilDate > ${Date.now() / 1000}`);
 
-  for (let pledge of activePledges) {
+  for (let pledge of entitledPledgesWithDiscord) {
     let userWithActivePledge = usersWithActivePledge.find(u => u.userId == pledge.discordUserId);
     
     // Update DB only if new pledge (update different tier only if new untilDate surpasses old untilDate, to avoid manual grant overridings)
