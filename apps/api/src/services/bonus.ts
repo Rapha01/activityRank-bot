1c7ccdc25734798a7aa9d4ee9faebd74daccdf40
piemot <84595516+piemot@users.noreply.github.com>
2026-01-08 11:46:27 -0800

    fix: add XP when assigning bonus via API

--- apps/api/src/services/bonus.ts
+++ apps/api/src/services/bonus.ts
@@ -20,26 +20,55 @@ export async function addBonusXP(guildId: string, userId: string, delta: number)
     return sql<number>`LEAST(${sql.lit(MAX_STAT_COLUMN_VALUE)}, GREATEST(${sql.lit(MIN_STAT_COLUMN_VALUE)}, ${expr}))`;
   }
 
-  await db
-    .insertInto('bonus')
-    .values((eb) => ({
-      guildId,
-      userId,
-      day: clamp(eb.val(delta)),
-      week: clamp(eb.val(delta)),
-      month: clamp(eb.val(delta)),
-      year: clamp(eb.val(delta)),
-      alltime: clamp(eb.val(delta)),
-      addDate: now,
-      changeDate: now,
-    }))
-    .onDuplicateKeyUpdate((eb) => ({
-      day: clamp(eb(eb.ref('day'), '+', values(eb.ref('day')))),
-      week: clamp(eb(eb.ref('week'), '+', values(eb.ref('week')))),
-      month: clamp(eb(eb.ref('month'), '+', values(eb.ref('month')))),
-      year: clamp(eb(eb.ref('month'), '+', values(eb.ref('year')))),
-      alltime: clamp(eb(eb.ref('month'), '+', values(eb.ref('alltime')))),
-      changeDate: now,
-    }))
-    .execute();
+  await db.transaction().execute(async (trx) => {
+    // NOTE: if another type of XP is being implemented xpPer must be fetched
+    // const xpPer = await trx
+    //   .selectFrom('guild')
+    //   .select(['xpPerBonus'])
+    //   .where('guildId', '=', guildId)
+    //   .executeTakeFirst();
+
+    await trx
+      .insertInto('bonus')
+      .values((eb) => ({
+        guildId,
+        userId,
+        day: clamp(eb.val(delta)),
+        week: clamp(eb.val(delta)),
+        month: clamp(eb.val(delta)),
+        year: clamp(eb.val(delta)),
+        alltime: clamp(eb.val(delta)),
+        addDate: now,
+        changeDate: now,
+      }))
+      .onDuplicateKeyUpdate((eb) => ({
+        day: clamp(eb(eb.ref('day'), '+', values(eb.ref('day')))),
+        week: clamp(eb(eb.ref('week'), '+', values(eb.ref('week')))),
+        month: clamp(eb(eb.ref('month'), '+', values(eb.ref('month')))),
+        year: clamp(eb(eb.ref('year'), '+', values(eb.ref('year')))),
+        alltime: clamp(eb(eb.ref('alltime'), '+', values(eb.ref('alltime')))),
+        changeDate: now,
+      }))
+      .execute();
+
+    await trx
+      .insertInto('guildMember')
+      .values((eb) => ({
+        guildId,
+        userId,
+        day: clamp(eb.val(delta)),
+        week: clamp(eb.val(delta)),
+        month: clamp(eb.val(delta)),
+        year: clamp(eb.val(delta)),
+        alltime: clamp(eb.val(delta)),
+      }))
+      .onDuplicateKeyUpdate((eb) => ({
+        day: clamp(eb(eb.ref('day'), '+', values(eb.ref('day')))),
+        week: clamp(eb(eb.ref('week'), '+', values(eb.ref('week')))),
+        month: clamp(eb(eb.ref('month'), '+', values(eb.ref('month')))),
+        year: clamp(eb(eb.ref('year'), '+', values(eb.ref('year')))),
+        alltime: clamp(eb(eb.ref('alltime'), '+', values(eb.ref('alltime')))),
+      }))
+      .execute();
+  });
 }
