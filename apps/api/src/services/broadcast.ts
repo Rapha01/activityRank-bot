d2c2547bd8a7c7873ac1483c370c6adee3a3bd35
piemot <84595516+piemot@users.noreply.github.com>
2026-01-15 19:59:19 -0800

    fix: send shared-guilds requests only once per host

--- apps/api/src/services/broadcast.ts
+++ apps/api/src/services/broadcast.ts
@@ -5,44 +5,51 @@ export async function broadcastRequest<T>(
   path: string,
   options: Parameters<typeof request>[1],
 ): Promise<
-  ({ ok: false; info?: string; shardId: number } | { ok: true; data: T; shardId: number })[]
+  ({ ok: false; info?: string; shardIds: number[] } | { ok: true; data: T; shardIds: number[] })[]
 > {
   const botInstances = await manager.db
+    .selectFrom('botShardStat')
+    .select(['ip'])
+    .distinct()
+    .execute();
+
+  const shardInstances = await manager.db
     .selectFrom('botShardStat')
     .select(['ip', 'shardId'])
     .execute();
 
   const promises = botInstances.map(async (instance) => {
-    console.debug('broadcasting to instance', instance);
+    const shardIds = shardInstances
+      .filter((inst) => inst.ip === instance.ip)
+      .map((shard) => shard.shardId);
 
     const parsedUrl = new URL(path, 'http://temp-hostname');
     parsedUrl.host = instance.ip;
     if (!parsedUrl.port) parsedUrl.port = '3010';
 
-    console.debug(`broadcasting to shard ${instance.shardId}, ${parsedUrl.toString()}`);
+    console.debug(`broadcasting to instance ${parsedUrl.toString()}`);
 
-    const { shardId } = instance;
     try {
       const response = await request(parsedUrl, options);
       if (response.statusCode !== 200) {
         console.debug(
-          `Error [${response.statusCode}] while broadcasting to shard=${shardId}`,
+          `Error [${response.statusCode}] while broadcasting to shard.ip=${parsedUrl.toString()}`,
           await response.body.text(),
         );
         return {
           ok: false as const,
           info: `[${response.statusCode}] ${await response.body.text()}`,
-          shardId,
+          shardIds,
         };
       }
       const data = await response.body.json();
-      return { ok: true as const, data: data as T, shardId };
+      return { ok: true as const, data: data as T, shardIds };
     } catch (err: any) {
-      console.debug(`Error while broadcasting to shard=${shardId}`, err);
+      console.debug(`Error while broadcasting to shard.ip=${parsedUrl.toString()}`, err);
       if ('code' in err && err.code === 'ECONNREFUSED') {
-        return { ok: false as const, info: `ECONNREFUSED "${parsedUrl}"`, shardId };
+        return { ok: false as const, info: `ECONNREFUSED "${parsedUrl}"`, shardIds };
       }
-      return { ok: false as const, info: err.toString(), shardId };
+      return { ok: false as const, info: err.toString(), shardIds };
     }
   });
 
