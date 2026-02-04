0c64d105cba9b219c95376618f04972630713b53
piemot <84595516+piemot@users.noreply.github.com>
2026-02-04 11:35:05 -0800

    chore: add a website beta user

--- apps/web/src/routes/dashboard/hasAccess.ts
+++ apps/web/src/routes/dashboard/hasAccess.ts
@@ -9,6 +9,7 @@ export async function hasAccess(userId: string): Promise<boolean> {
   const TESTERS = [
     '772394467339534336', // @toptashka
     '587294586452443136', // @jac8x
+    '749555128394055702', // @mewowieee
   ];
 
   if (STAFF.includes(userId)) return true;
